25923c387d61f1808dd246e581bd46b8eaf52e0f
gyheo <heo.geoyeo@gmail.com>
2020-03-06 00:12:57 +0900

    Initial commit for creating react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Compo from "./Compo";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello React with TypeScript</h1>
+      <Compo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Compo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Compo() {
+    return <h1>Make a first component!</h1>;
+}
+
+export default Compo;
